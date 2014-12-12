8ea803973e7feb8bfa4480cfb76214b4eef3c56e
kitlloyd@gmail.com <kitlloyd@Kits-MacBook-Air-3.local>
2014-12-12 12:31:39 +0400

    deleted a comment on line 3

--- Kitstest.R
+++ Kitstest.R
@@ -1,6 +1,5 @@
 setwd("~/Dropbox/Courses/Data Science/Getting and Cleaning Data")
 fileURL <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-# Local data file hjhhj
 # Local data file
 dataFileZIP <- "./getdata-projectfiles-UCI-HAR-Dataset.zip"
 
